4e3906a443b8855d029a397ddf2721aa8accf604
AlexNK001 <126164280+AlexNK001@users.noreply.github.com>
2025-01-31 14:20:43 +0300

    Weapon corrected

--- Weapon/Weapon/Program.cs
+++ Weapon/Weapon/Program.cs
@@ -7,27 +7,42 @@ namespace Weapon
         private static void Main()
         {
             Player player = new Player(health: 10);
-            Weapon weapon = new Weapon(damage: 5);
-            Bot bot = new Bot(weapon, bulletsCount: 3);
+            Weapon weapon = new Weapon(damage: 5, bulletsCount: 3);
+            Bot bot = new Bot(weapon);
             bot.OnSeePlayer(player);
         }
     }
 
     public class Weapon
     {
-        public Weapon(int damage)
+        private readonly int _damage;
+        private int _bullets;
+
+        public Weapon(int damage, int bulletsCount)
         {
             if (damage > 0)
+                _damage = damage;
+            else
+                throw new ArgumentOutOfRangeException();
+
+            if (bulletsCount > 0)
+                _bullets = bulletsCount;
+            else
+                throw new ArgumentOutOfRangeException();
+        }
+
+        public void Fire(Player player)
+        {
+            if (_bullets > 0)
             {
-                Damage = damage;
+                player.TakeDamage(_damage);
+                _bullets--;
             }
             else
             {
                 throw new InvalidOperationException();
             }
         }
-
-        public int Damage { get; }
     }
 
     public class Player
@@ -37,53 +52,35 @@ namespace Weapon
         public Player(int health)
         {
             if (health > 0)
-            {
                 _health = health;
-            }
             else
-            {
-                throw new InvalidOperationException();
-            }
+                throw new ArgumentOutOfRangeException();
         }
 
         public void TakeDamage(int damage)
         {
-            int minDamage = 0;
-            _health -= Math.Max(damage, minDamage);
+            if (damage > 0)
+                _health -= damage;
+            else
+                throw new ArgumentOutOfRangeException();
         }
     }
 
     public class Bot
     {
         private readonly Weapon _weapon;
-        private int _bullets;
 
-        public Bot(Weapon weapon, int bulletsCount)
+        public Bot(Weapon weapon)
         {
-            if (_bullets > 0)
-            {
-                _bullets = bulletsCount;
-            }
+            if (weapon != null)
+                _weapon = weapon;
             else
-            {
-                throw new InvalidOperationException();
-            }
-            
-            _weapon = weapon;
+                throw new ArgumentNullException();
         }
 
         public void OnSeePlayer(Player player)
         {
-            if (_bullets > 0)
-            {
-                Fire(player);
-            }
-        }
-
-        private void Fire(Player player)
-        {
-            player.TakeDamage(_weapon.Damage);
-            _bullets--;
+            _weapon.Fire(player);
         }
     }
 }
