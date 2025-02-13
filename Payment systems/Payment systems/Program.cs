80a8e74421110227b012f5630f49f53acb9b3fb2
AlexNK001 <126164280+AlexNK001@users.noreply.github.com>
2025-02-13 18:58:22 +0300

    Payment systems corrected

--- Payment systems/Payment systems/Program.cs
+++ Payment systems/Payment systems/Program.cs
@@ -1,28 +1,18 @@
 ﻿using System;
-using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 
 public class Program
 {
     private static void Main()
     {
-        Order order = new Order(id: 77, amount: 5);
+        Order order = new Order(id: 77, amount: 12000);
 
         IPaymentSystem[] paymentSystems = new IPaymentSystem[]
         {
-            new PaymentSystem(
-                WebsiteStorage.System1, 
-                LinkFabric.HashComponents.CreateMD5()),
-
-            new PaymentSystem(
-                WebsiteStorage.System2, 
-                LinkFabric.HashComponents.CreateMD5(),
-                LinkFabric.OrderBasedComponent.CreateSumBasedComponent()),
-
-            new PaymentSystem(
-                WebsiteStorage.System3,
-                LinkFabric.HashComponents.CreateSHA1(),
-                LinkFabric.OrderBasedComponent.CreateSumIDComponent(),
-                LinkFabric.Security.KeySystem())
+            new PaySystem1(new HashGeneratorMD5()),
+            new PaySystem2(new HashGeneratorMD5()),
+            new PaySystem3(new HashGeneratorSHA1(), key: 111)
         };
 
         for (int i = 0; i < paymentSystems.Length; i++)
@@ -34,9 +24,6 @@ public class Order
 {
     public Order(int id, int amount)
     {
-        if (id <= 0)
-            throw new InvalidOperationException(nameof(id));
-
         if (amount < 0)
             throw new ArgumentOutOfRangeException(nameof(amount));
 
@@ -48,32 +35,59 @@ public class Order
     public int Amount { get; }
 }
 
-interface IPaymentSystem
+public interface IPaymentSystem
 {
     string GetPayingLink(Order order);
 }
 
-public interface ILinkComponent
+public interface IHashGenerator
 {
-    string GetPartLink(Order order);
+    string GetHash(int value);
 }
 
-public class PaymentSystem : IPaymentSystem
+public class HashGeneratorMD5 : IHashGenerator
 {
-    private readonly ILinkComponent[] _linkComponents;
-    private readonly string _website;
+    private readonly MD5 _md5;
 
-    public PaymentSystem(string website, params ILinkComponent[] linkComponents)
+    public HashGeneratorMD5()
     {
-        _website = website ?? throw new ArgumentNullException(nameof(website));
+        _md5 = MD5.Create();
+    }
 
-        if (linkComponents == null)
-            throw new ArgumentNullException(nameof(linkComponents));
+    public string GetHash(int value)
+    {
+        byte[] bytes = Encoding.UTF8.GetBytes(value.ToString());
+        byte[] hash = _md5.ComputeHash(bytes);
 
-        if (linkComponents.Any(component => component == null))
-            throw new ArgumentNullException($"{nameof(linkComponents)} contains an element equal to null");
+        return Encoding.UTF8.GetString(hash);
+    }
+}
 
-        _linkComponents = linkComponents;
+public class HashGeneratorSHA1 : IHashGenerator
+{
+    private readonly SHA1 _sha1;
+
+    public HashGeneratorSHA1()
+    {
+        _sha1 = SHA1.Create();
+    }
+
+    public string GetHash(int value)
+    {
+        byte[] bytes = Encoding.UTF8.GetBytes(value.ToString());
+        byte[] hash = _sha1.ComputeHash(bytes);
+
+        return Encoding.UTF8.GetString(hash);
+    }
+}
+
+public class PaySystem1 : IPaymentSystem
+{
+    private readonly IHashGenerator _hashGenerator;
+
+    public PaySystem1(IHashGenerator hashGenerator)
+    {
+        _hashGenerator = hashGenerator ?? throw new ArgumentNullException(nameof(hashGenerator));
     }
 
     public string GetPayingLink(Order order)
@@ -81,138 +95,48 @@ public class PaymentSystem : IPaymentSystem
         if (order == null)
             throw new ArgumentNullException(nameof(order));
 
-        string link = _website;
+        if (order.Amount <= 0)
+            throw new InvalidOperationException();
 
-        for (int i = 0; i < _linkComponents.Length; i++)
-            link += _linkComponents[i].GetPartLink(order);
-
-        return link;
+        return $"pay.system1.ru/order?amount={order.Amount}RUB&hash={_hashGenerator.GetHash(order.Id)}";
     }
 }
 
-public class HashLinkComponent : ILinkComponent
+public class PaySystem2 : IPaymentSystem
 {
-    private readonly string _name;
+    private readonly IHashGenerator _hashGenerator;
 
-    public HashLinkComponent(string name)
+    public PaySystem2(IHashGenerator hashGenerator)
     {
-        _name = name ?? throw new ArgumentNullException(nameof(name));
+        _hashGenerator = hashGenerator ?? throw new ArgumentNullException(nameof(hashGenerator));
     }
 
-    public string GetPartLink(Order order)
+    public string GetPayingLink(Order order)
     {
         if (order == null)
             throw new ArgumentNullException(nameof(order));
 
-        return $"{_name} {order.GetHashCode()}";
+        if (order.Amount <= 0)
+            throw new InvalidOperationException();
+
+        return $"pay.system1.ru/order?amount={order.Amount}RUB&hash={_hashGenerator.GetHash(order.Id) + order.Amount}";
     }
 }
 
-public class SumBasedComponent : ILinkComponent
+public class PaySystem3 : IPaymentSystem
 {
-    private readonly string _name;
-
-    public SumBasedComponent(string name)
-    {
-        _name = name ?? throw new ArgumentNullException(nameof(name));
-    }
-
-    public string GetPartLink(Order order)
-    {
-        if (order == null)
-            throw new ArgumentNullException(nameof(order));
-
-        return $"{_name} {order.Amount.GetHashCode()}";
-    }
-}
-
-public class SumIDComponent : ILinkComponent
-{
-    private readonly string _name;
-
-    public SumIDComponent(string name)
-    {
-        _name = name ?? throw new ArgumentNullException(nameof(name));
-    }
-
-    public string GetPartLink(Order order)
-    {
-        if (order == null)
-            throw new ArgumentNullException(nameof(order));
-
-        return $"{_name} {order.Id.GetHashCode()}";
-    }
-}
-
-public class KeySystem : ILinkComponent
-{
-    private readonly string _name;
+    private readonly IHashGenerator _hashGenerator;
     private readonly int _key;
 
-    public KeySystem(string name, int key)
+    public PaySystem3(IHashGenerator hashGenerator, int key)
     {
-        _name = name ?? throw new ArgumentNullException(nameof(name));
-
-        if (key == 0 && key == 1)
-            throw new InvalidOperationException($"{key} does not fit as a key.");
-
+        _hashGenerator = hashGenerator ?? throw new ArgumentNullException(nameof(hashGenerator));
         _key = key;
     }
 
-    public string GetPartLink(Order order)
+    public string GetPayingLink(Order order)
     {
-        if (order == null)
-            throw new ArgumentNullException(nameof(order));
-
-        int key = (order.Id + order.Amount) * _key;
-
-        return $"{_name} {key}";
+        return $"system3.com/pay?amount={order.Amount}&curency=RUB&hash=" +
+            $"{_hashGenerator.GetHash(order.Amount)}{order.Id}{_key}";
     }
 }
-
-public static class LinkFabric
-{
-    public static class HashComponents
-    {
-        public static ILinkComponent CreateMD5()
-        {
-            return new HashLinkComponent("MD5");
-        }
-
-        public static ILinkComponent CreateSHA1()
-        {
-            return new HashLinkComponent("SHA1");
-        }
-    }
-
-    public static class OrderBasedComponent
-    {
-        public static ILinkComponent CreateSumBasedComponent()
-        {
-            return new SumBasedComponent("SumBasedComponent");
-        }
-
-        public static ILinkComponent CreateSumIDComponent()
-        {
-            return new SumIDComponent("SumIDComponent");
-        }
-    }
-
-    public static class Security
-    {
-        public static ILinkComponent KeySystem()
-        {
-            return new KeySystem("KeySystem", key: 3);
-        }
-    }
-}
-
-public static class WebsiteStorage
-{
-    public const string System1 = "pay.system1.ru/order?amount=12000RUB&hash=";
-    public const string System2 = "order.system2.ru/pay?hash=";
-    public const string System3 = "system3.com/pay?amount=12000&curency=RUB&hash=";
-}
-
-
-
