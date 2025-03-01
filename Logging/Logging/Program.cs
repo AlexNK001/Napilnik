24fa9d7fbaad7442c8a6f84858117ddeb8077301
AlexNK001 <126164280+AlexNK001@users.noreply.github.com>
2025-03-01 11:44:59 +0300

    correct 2

--- Logging/Logging/Program.cs
+++ Logging/Logging/Program.cs
@@ -1,227 +1,117 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
 using System.IO;
 using System.Reflection;
 using System.Security.Cryptography;
 using System.Text;
 
-public class Passport
+public static class Program
 {
-    private const int PassportDataLength = 10;
-
-    public Passport(string rawData)
+    private static void Main()
     {
-        if (rawData == null)
-            throw new ArgumentNullException(nameof(rawData));
-
-        if (CanData(rawData))
-            Data = rawData;
-        else
-            throw new InvalidOperationException(MessageStorage.InvalidInput);
-    }
-
-    public string Data { get; }
-
-    public static bool CanData(string data)
-    {
-        if (data == null)
-            throw new ArgumentNullException(nameof(data));
-
-        return data.Trim().Replace(" ", string.Empty).Length == PassportDataLength;
-    }
-}
-
-public static class MessageStorage
-{
-    public const string AccessGranted = "По паспорту «{0}» доступ к бюллетеню на дистанционном электронном голосовании ПРЕДОСТАВЛЕН";
-    public const string AccessNotGranted = "По паспорту «{0}» доступ к бюллетеню на дистанционном электронном голосовании НЕ ПРЕДОСТАВЛЯЛСЯ";
-    public const string PassportNotFound = "Паспорт «{0}» в списке участников дистанционного голосования НЕ НАЙДЕН";
-
-    public const string EnteringPassportData = "Введите серию и номер паспорта";
-    public const string InvalidInput = "Неверный формат серии или номера паспорта";
-
-    public const string FileNotFound = "Файл db.sqlite не найден. Положите файл в папку вместе с exe.";
-}
-
-public class View
-{
-    private readonly Dictionary<MessageToPassport, string> _messageToPassport;
-    private readonly Dictionary<MessageToResult, string> _messageToResult;
-
-    private readonly TextBox _passport;
-    private readonly TextBox _result;
-    private readonly TextBox _message;
-    private readonly Presenter _presenter;
-
-    public View(TextBox passport, TextBox result, TextBox message, Presenter presenter)
-    {
-        _passport = passport ?? throw new ArgumentNullException(nameof(passport));
-        _result = result ?? throw new ArgumentNullException(nameof(result));
-        _message = message ?? throw new ArgumentNullException(nameof(message));
-        _presenter = presenter ?? throw new ArgumentNullException(nameof(presenter));
-
-        _messageToPassport = new Dictionary<MessageToPassport, string>
-        {
-            { MessageToPassport.AccessGranted, MessageStorage.AccessGranted },
-            { MessageToPassport.AccessNotGranted, MessageStorage.AccessNotGranted },
-            { MessageToPassport.PassportNotFound, MessageStorage.PassportNotFound }
-        };
-
-        _messageToResult = new Dictionary<MessageToResult, string>
-        {
-            { MessageToResult.EnteringPassportData, MessageStorage.EnteringPassportData },
-            { MessageToResult.InvalidInput, MessageStorage.InvalidInput }
-        };
-    }
-
-    private void OnClick(object sender, EventArgs e)
-    {
-        _presenter.SetRawData(_passport.Text);
-    }
-
-    public void ShowPassport(MessageToPassport status, string text)
-    {
-        if (_messageToPassport.ContainsKey(status) == false)
-            throw new ArgumentOutOfRangeException(nameof(status));
-
-        if (text == null)
-            throw new ArgumentNullException(nameof(text));
-
-        _passport.Text = string.Format(_messageToPassport[status], text);
-    }
-
-    public void ShowResult(MessageToResult result)
-    {
-        if (_messageToResult.ContainsKey(result) == false)
-            throw new ArgumentOutOfRangeException(nameof(result));
-
-        _result.Text = _messageToResult[result];
-    }
-
-    public void ShowFileNotFound()
-    {
-        _message.Text = MessageStorage.FileNotFound;
-    }
-}
-
-public class Model
-{
-    private const int ErrorCode = 1;
-
-    private HashCreator _hashCreator;
-
-    public Model(HashCreator hashCreator)
-    {
-        _hashCreator = hashCreator ?? throw new ArgumentNullException(nameof(hashCreator));
-    }
-
-    public bool TryFindPassportToBD(Passport passport, out MessageToPassport message)
-    {
-        string location = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-
-        try
-        {
-            var connection = CreateConnection(location, passport.Data);
-            var sqLiteDataAdapter = CreateAdapter(connection);
-            var dataTable = new DataTable();
-
-            connection.Open();
-            sqLiteDataAdapter.Fill(dataTable);
-            connection.Close();
-
-            message = GetMessage(dataTable);
-
-            return true;
-        }
-        catch
-        {
-            message = MessageToPassport.none;
-            return false;
-        }
-    }
-
-    public bool TryCreatePassport(string text, out Passport passport, out MessageToResult passportMessage)
-    {
-        if (text == null)
-            throw new ArgumentNullException(nameof(text));
-
-        if (Passport.CanData(text))
-        {
-            passport = new Passport(text);
-            passportMessage = MessageToResult.none;
-            return true;
-        }
-        else
-        {
-            passport = null;
-            passportMessage = text == string.Empty ? MessageToResult.InvalidInput : MessageToResult.EnteringPassportData;
-            return false;
-        }
-    }
-
-    private static MessageToPassport GetMessage(DataTable dataTable)
-    {
-        MessageToPassport message;
-
-        if (dataTable.Rows.Count > 0)
-        {
-            bool isConvert = Convert.ToBoolean(dataTable.Rows[0].ItemArray[1]);
-            message = isConvert ? MessageToPassport.AccessGranted : MessageToPassport.AccessNotGranted;
-        }
-        else
-        {
-            message = MessageToPassport.PassportNotFound;
-        }
-
-        return message;
-    }
-
-    private dynamic CreateConnection(string location, string rawData)
-    {
-        string connectionString = $"Data Source={location}\\db.sqlite";
-        string hash = _hashCreator.ComputeSha256Hash(rawData);
-        string commandText = $"select * from passports where num='{hash}' limit 1;";
-
-        return new SQLiteConnection(connectionString);
-    }
-
-    private dynamic CreateAdapter(dynamic connection)
-    {
-        var liteCommand = new SQLiteCommand(commandText, connection);
-
-        return new SQLiteDataAdapter(liteCommand);
+        ViewFabric viewFabric = new ViewFabric();
+        DatabaseContext databaseContext = new DatabaseContext();
+        HashCreator hashCreator = new HashCreator();
+        Repository repository = new Repository(databaseContext, hashCreator);
+        Presenter presenter = new Presenter(viewFabric, repository);
     }
 }
 
 public class Presenter
 {
     private readonly View _view;
-    private readonly Model _model;
+    private readonly Repository _repository;
 
-    public Presenter(View view, Model model)
+    public Presenter(ViewFabric viewFabric, Repository repository)
     {
-        _view = view ?? throw new ArgumentNullException(nameof(view));
-        _model = model ?? throw new ArgumentNullException(nameof(model));
+        if (viewFabric == null)
+            throw new ArgumentNullException(nameof(viewFabric));
+        else
+            _view = viewFabric.CreateView(this);
+
+        _repository = repository ?? throw new ArgumentNullException(nameof(repository));
     }
 
-    public void SetRawData(string text)
+    public void HandleRawData(string text)
     {
-        if (text == null)
+        if (string.IsNullOrWhiteSpace(text))
             throw new ArgumentNullException(nameof(text));
 
-        if (_model.TryCreatePassport(text, out Passport passport, out MessageToResult passportMessage))
-            FindPassport(passport);
-        else
-            _view.ShowResult(passportMessage);
+        try
+        {
+            Passport passport = new Passport(text);
+            Citizen citizen = _repository.FindCitezen(passport);
+
+            string message;
+
+            if (citizen == null)
+                message = MessageStorage.EnteringPassportData;
+            else
+                message = citizen.IsVoted ? MessageStorage.AccessGranted : MessageStorage.AccessNotGranted;
+
+            _view.ShowPassport(message, passport.Data);
+        }
+        catch (Exception exception)
+        {
+            _view.ShowException(exception.Message);
+        }
+    }
+}
+
+public class Repository
+{
+    private readonly DatabaseContext _databaseContext;
+    private readonly HashCreator _hashCreator;
+
+    public Repository(DatabaseContext databaseContext, HashCreator hashCreator)
+    {
+        _databaseContext = databaseContext ?? throw new ArgumentNullException(nameof(databaseContext));
+        _hashCreator = hashCreator ?? throw new ArgumentNullException(nameof(hashCreator));
     }
 
-    private void FindPassport(Passport passport)
+    public Citizen FindCitezen(Passport passport)
     {
-        if (_model.TryFindPassportToBD(passport, out MessageToPassport message))
-            _view.ShowPassport(message, passport.Data);
+        string hash = _hashCreator.ComputeSha256Hash(passport.Data);
+        DataTable dataTable = _databaseContext.CreateDataTable(hash);
+
+        if (dataTable.Rows.Count > 0)
+        {
+            bool isVoted = Convert.ToBoolean(dataTable.Rows[0].ItemArray[1]);
+            return new Citizen(isVoted);
+        }
         else
-            _view.ShowFileNotFound();
+        {
+            return null;
+        }
+    }
+}
+
+public class DatabaseContext
+{
+    public DataTable CreateDataTable(string hash)
+    {
+        try
+        {
+            string commandText = string.Format("select * from passports where num='{0}' limit 1;", hash);
+            string location = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string connectionString = string.Format("Data Source={0}\\db.sqlite", location);
+
+            SQLiteConnection connection = new SQLiteConnection(connectionString);
+            connection.Open();
+            SQLiteCommand sqLiteCommand = new SQLiteCommand(commandText, connection);
+            SQLiteDataAdapter sqLiteDataAdapter = new SQLiteDataAdapter(sqLiteCommand);
+            DataTable dataTable = new DataTable();
+            sqLiteDataAdapter.Fill(dataTable);
+            connection.Close();
+            return dataTable;
+        }
+        catch (SQLiteException exception)
+        {
+            if (exception.ErrorCode != 1)
+                throw new FileNotFoundException("Файл db.sqlite не найден. Положите файл в папку вместе с exe.");
+
+            return new DataTable();
+        }
     }
 }
 
@@ -236,19 +126,87 @@ public class HashCreator
     }
 }
 
-public enum MessageToPassport
+public class Citizen
 {
-    none,
-    AccessGranted,
-    AccessNotGranted,
-    PassportNotFound
+    public Citizen(bool isVoted)
+    {
+        IsVoted = isVoted;
+    }
+
+    public bool IsVoted { get; }
 }
 
-public enum MessageToResult
+public class Passport
 {
-    none,
-    EnteringPassportData,
-    InvalidInput
+    private const int PassportDataLength = 10;
+
+    public Passport(string rawData)
+    {
+        if (string.IsNullOrWhiteSpace(rawData))
+            throw new ArgumentNullException(nameof(rawData));
+
+        string data = rawData.Trim().Replace(" ", string.Empty);
+
+        if (data.Length == PassportDataLength)
+            Data = data;
+        else
+            throw new InvalidOperationException(MessageStorage.InvalidInput);
+    }
+
+    public string Data { get; }
+}
+
+public class View
+{
+    private readonly TextBox _passport;
+    private readonly TextBox _message;
+    private readonly Presenter _presenter;
+
+    public View(TextBox passport, TextBox message, Presenter presenter)
+    {
+        _passport = passport ?? throw new ArgumentNullException(nameof(passport));
+        _message = message ?? throw new ArgumentNullException(nameof(message));
+        _presenter = presenter ?? throw new ArgumentNullException(nameof(presenter));
+    }
+
+    private void OnClick(object sender, EventArgs e)
+    {
+        _presenter.HandleRawData(_passport.Text);
+    }
+
+    public void ShowPassport(string status, string passportData)
+    {
+        if (string.IsNullOrWhiteSpace(status))
+            throw new ArgumentNullException(nameof(status));
+
+        if (string.IsNullOrWhiteSpace(passportData))
+            throw new ArgumentNullException(nameof(passportData));
+
+        _passport.Text = string.Format(status, passportData);
+    }
+
+    public void ShowException(string errorMessage)
+    {
+        _message.Text = errorMessage;
+    }
+}
+
+public class ViewFabric
+{
+    public View CreateView(Presenter presenter)
+    {
+        return new View(new TextBox(), new TextBox(), presenter);
+    }
+}
+
+public static class MessageStorage
+{
+    public const string AccessGranted = "По паспорту «{0}» доступ к бюллетеню на дистанционном электронном голосовании ПРЕДОСТАВЛЕН";
+    public const string AccessNotGranted = "По паспорту «{0}» доступ к бюллетеню на дистанционном электронном голосовании НЕ ПРЕДОСТАВЛЯЛСЯ";
+    public const string PassportNotFound = "Паспорт «{0}» в списке участников дистанционного голосования НЕ НАЙДЕН";
+    public const string EnteringPassportData = "Введите серию и номер паспорта";
+    public const string InvalidInput = "Неверный формат серии или номера паспорта";
+    public const string FileNotFound = "Файл db.sqlite не найден. Положите файл в папку вместе с exe.";
 }
 
 public class TextBox
